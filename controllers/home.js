77f800093d50f220c508d258625c36b791398991
Manuel Villegas <manuel.villegas.michel@gmail.com>
2023-10-30 13:26:55 -0500

    Comments for each post

--- controllers/home.js
+++ controllers/home.js
@@ -3,13 +3,8 @@ const { models } = require('../models/models');
 module.exports.main = async (req, res, next) => {
   try {
     const certifications = await models.Certifications.findAll({ limit: 12 });
-    console.log(certifications);
     res.render('home', { certifications });
   } catch (error) {
     next(error);
   }
 };
-
-// module.exports.homeControllers = {
-//   main,
-// };
--- routes/certifications.js
+++ routes/certifications.js
@@ -2,7 +2,6 @@ const express = require('express');
 const Router = express.Router();
 const certificationsControllers = require('../controllers/certifications');
 
-module.exports = Router.get('/', certificationsControllers.show).get(
-  '/:id',
-  certificationsControllers.renderDetailsPage
-);
+module.exports = Router.get('/', certificationsControllers.show)
+  .get('/:id', certificationsControllers.renderDetailsPage)
+  .post('/:id', certificationsControllers.newReview);
